54f3990b7eeba9a987a249c9e2b2e704c85a3d27
JoseMurillo <joselillo072545@gmail.com>
2022-10-02 20:34:50 -0500

    Sin dev Online

--- main.js
+++ main.js
@@ -37,4 +37,11 @@ function main(){
 
     console.log(usuario1.getUserName);
     console.log(usuario1.getkey);
+
+    function encryptar () {
+        lista = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+
+        //como saber la posicion del elemento x
+    }
+
 }
